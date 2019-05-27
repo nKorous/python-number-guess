3366143a7916bfa9640b98eb7f373dd464847e9a
Nate Korous <natekorous@nathaniels-mbp.localdomain>
2019-05-26 20:59:00 -0600

    created main and readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Number Guesser
+
+###Explination
+Simple number guessing app. Enter an integer between 0 and 100 and see how close you are. Try and guess the number with the distance clues that are given.
+
+## Run
+
+`python3 main.py`
+
+## Feedback is welcome, it's been a few years since i didnt anything with python.
---
+++ main.py
@@ -0,0 +1,105 @@
+#!/usr/local/bin/python3
+
+##TODO:
+##1. make sure input is int
+##2. make sure empty(none, null) is handled
+##3. try, catch, except
+
+from random import randint
+import sys
+
+answer = 0
+chance = 0
+minNum = 0
+maxNum = 100
+pastGuesses = ["Chance, Guess, Actual Distance"]
+
+def randNum(min, max):
+    return randint(min, max)
+
+def guessDistance(a, g):
+    d = a - g
+    return abs(d)
+    
+def incChance():
+    global chance
+    chance += 1
+
+def logGuess(g, d):
+    global pastGuesses
+    global chance
+    pastGuesses.append(f"{chance}, {g}, {d}")
+
+def incorrectGuess(g, d, m):
+    global answer
+    global minNum
+    global maxNum
+    global chance
+    incChance()
+    logGuess(g, d)
+    print(f"Guess incorrect, Please try again")
+    x = input(f"Your last guess of {g} was {m} of the answer. Guess another number between {minNum} and {maxNum}: ")
+    isInt(x)
+
+def checkGuess(a, g):
+    global chance
+    global pastGuesses
+    dist = guessDistance(a, g)
+    if dist == 0:
+        incChance()
+        logGuess(g, dist)
+        print(f"Congratulations, your guess of {g} was correct!")
+        print(f"You got the answer in {chance} chances! Here is how this played out:  \n")
+        for c in pastGuesses:
+            print(c, sep=',')
+
+        sys.exit() #exits program
+    elif dist > 50:
+        incorrectGuess(g, dist, 'more than 50 away')
+    elif dist <= 2:
+        incorrectGuess(g, dist, 'within 2')
+    elif dist <= 5:
+        incorrectGuess(g, dist, 'within 5')
+    elif dist <= 10:
+        incorrectGuess(g, dist, 'within 10')
+    elif dist <= 15:
+        incorrectGuess(g, dist, 'within 15')
+    elif dist <= 25:
+        incorrectGuess(g, dist, 'within 25')
+    elif dist <= 50:
+        incorrectGuess(g, dist, 'within 50')
+
+
+
+def guessInRange(guess):
+    global minNum
+    global maxNum
+    global answer
+    if(guess < minNum or guess > maxNum):
+        guess = input(f"{guess} is not between {minNum} and {maxNum}. Please try again: ")
+        isInt(guess)
+    else:
+        checkGuess(answer, guess)
+
+def isInt(g):
+    global minNum
+    global maxNum
+    try:
+        int(g)
+    except:
+        guess = input(f"{g} is not a valid integer, please enter a valid integer between {minNum} and {maxNum}: ")
+        isInt(guess)
+    
+    return guessInRange(int(g))
+
+
+def main():
+    global answer
+    global minNum
+    global maxNum
+    guess = input(f"Type a number between {minNum} and {maxNum}: ")
+    answer = randNum(minNum, maxNum) #The random number that is generated to guess against
+    isInt(guess)
+
+if __name__ == "__main__":
+    main()
